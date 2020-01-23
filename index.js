99a252ca66173fc42b63a810e7cab1ad054f2978
Ike <ikescode@gmail.com>
2020-01-23 11:51:50 -0800

    add mousemove handler to move paddle with x on mouse

--- index.js
+++ index.js
@@ -55,8 +55,17 @@ function keyUpHandler(e) {
   }
 }
 
+function mouseMoveHandler(e) {
+  const relativeX = e.clientX - canvas.offsetLeft;
+  if (relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth / 2;
+  }
+}
+
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
+document.addEventListener('mousemove', mouseMoveHandler, false);
+
 
 function drawPaddle() {
   ctx.beginPath();
